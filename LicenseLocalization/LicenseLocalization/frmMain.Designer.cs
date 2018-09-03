e1fbd82667173e0d792420bd5a16e0723a650624
Sect1on <41570859+Sect1on@users.noreply.github.com>
2018-09-03 18:31:43 +0800

    LicenseLocalization

---
+++ LicenseLocalization/LicenseLocalization/frmMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace LicenseLocalization
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOrigin = new System.Windows.Forms.PictureBox();
+            this.picRec = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picLicense = new System.Windows.Forms.PictureBox();
+            this.picLaststep = new System.Windows.Forms.PictureBox();
+            this.piccha1 = new System.Windows.Forms.PictureBox();
+            this.piccha2 = new System.Windows.Forms.PictureBox();
+            this.piccha3 = new System.Windows.Forms.PictureBox();
+            this.piccha4 = new System.Windows.Forms.PictureBox();
+            this.piccha5 = new System.Windows.Forms.PictureBox();
+            this.piccha6 = new System.Windows.Forms.PictureBox();
+            this.piccha7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOrigin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRec)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLicense)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLaststep)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOrigin
+            // 
+            this.picOrigin.Location = new System.Drawing.Point(12, 12);
+            this.picOrigin.Name = "picOrigin";
+            this.picOrigin.Size = new System.Drawing.Size(400, 300);
+            this.picOrigin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOrigin.TabIndex = 0;
+            this.picOrigin.TabStop = false;
+            // 
+            // picRec
+            // 
+            this.picRec.Location = new System.Drawing.Point(418, 12);
+            this.picRec.Name = "picRec";
+            this.picRec.Size = new System.Drawing.Size(400, 300);
+            this.picRec.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRec.TabIndex = 1;
+            this.picRec.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(836, 595);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picLicense
+            // 
+            this.picLicense.Location = new System.Drawing.Point(418, 318);
+            this.picLicense.Name = "picLicense";
+            this.picLicense.Size = new System.Drawing.Size(400, 121);
+            this.picLicense.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLicense.TabIndex = 3;
+            this.picLicense.TabStop = false;
+            // 
+            // picLaststep
+            // 
+            this.picLaststep.Location = new System.Drawing.Point(12, 318);
+            this.picLaststep.Name = "picLaststep";
+            this.picLaststep.Size = new System.Drawing.Size(400, 300);
+            this.picLaststep.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLaststep.TabIndex = 4;
+            this.picLaststep.TabStop = false;
+            // 
+            // piccha1
+            // 
+            this.piccha1.Location = new System.Drawing.Point(418, 445);
+            this.piccha1.Name = "piccha1";
+            this.piccha1.Size = new System.Drawing.Size(60, 121);
+            this.piccha1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha1.TabIndex = 5;
+            this.piccha1.TabStop = false;
+            // 
+            // piccha2
+            // 
+            this.piccha2.Location = new System.Drawing.Point(484, 445);
+            this.piccha2.Name = "piccha2";
+            this.piccha2.Size = new System.Drawing.Size(60, 121);
+            this.piccha2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha2.TabIndex = 6;
+            this.piccha2.TabStop = false;
+            // 
+            // piccha3
+            // 
+            this.piccha3.Location = new System.Drawing.Point(550, 445);
+            this.piccha3.Name = "piccha3";
+            this.piccha3.Size = new System.Drawing.Size(60, 121);
+            this.piccha3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha3.TabIndex = 7;
+            this.piccha3.TabStop = false;
+            // 
+            // piccha4
+            // 
+            this.piccha4.Location = new System.Drawing.Point(616, 445);
+            this.piccha4.Name = "piccha4";
+            this.piccha4.Size = new System.Drawing.Size(60, 121);
+            this.piccha4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha4.TabIndex = 8;
+            this.piccha4.TabStop = false;
+            // 
+            // piccha5
+            // 
+            this.piccha5.Location = new System.Drawing.Point(682, 445);
+            this.piccha5.Name = "piccha5";
+            this.piccha5.Size = new System.Drawing.Size(60, 121);
+            this.piccha5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha5.TabIndex = 9;
+            this.piccha5.TabStop = false;
+            // 
+            // piccha6
+            // 
+            this.piccha6.Location = new System.Drawing.Point(748, 445);
+            this.piccha6.Name = "piccha6";
+            this.piccha6.Size = new System.Drawing.Size(60, 121);
+            this.piccha6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha6.TabIndex = 10;
+            this.piccha6.TabStop = false;
+            // 
+            // piccha7
+            // 
+            this.piccha7.Location = new System.Drawing.Point(814, 445);
+            this.piccha7.Name = "piccha7";
+            this.piccha7.Size = new System.Drawing.Size(60, 121);
+            this.piccha7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piccha7.TabIndex = 11;
+            this.piccha7.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 637);
+            this.Controls.Add(this.piccha7);
+            this.Controls.Add(this.piccha6);
+            this.Controls.Add(this.piccha5);
+            this.Controls.Add(this.piccha4);
+            this.Controls.Add(this.piccha3);
+            this.Controls.Add(this.piccha2);
+            this.Controls.Add(this.piccha1);
+            this.Controls.Add(this.picLaststep);
+            this.Controls.Add(this.picLicense);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picRec);
+            this.Controls.Add(this.picOrigin);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.picOrigin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRec)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLicense)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLaststep)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piccha7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOrigin;
+        private System.Windows.Forms.PictureBox picRec;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picLicense;
+        private System.Windows.Forms.PictureBox picLaststep;
+        private System.Windows.Forms.PictureBox piccha1;
+        private System.Windows.Forms.PictureBox piccha2;
+        private System.Windows.Forms.PictureBox piccha3;
+        private System.Windows.Forms.PictureBox piccha4;
+        private System.Windows.Forms.PictureBox piccha5;
+        private System.Windows.Forms.PictureBox piccha6;
+        private System.Windows.Forms.PictureBox piccha7;
+    }
+}
+
---
+++ LicenseLocalization/LicenseLocalization/frmMain.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;                        
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.Util;
+using Emgu.CV.UI;
+
+namespace LicenseLocalization
+{
+    public partial class frmMain : Form
+    {
+        Image<Bgr, byte> OriginPic = new Image<Bgr, byte>(@"..\..\..\Pictures\1.jpg");//从文件加载图片
+
+        //图片大小320*240像素，格式jpg
+        Image<Bgr, byte> Transferpic;
+        Image<Bgr, byte> Licensepic;
+        Image<Bgr, byte> chapic1;
+        Image<Bgr, byte> chapic2;
+        Image<Bgr, byte> chapic3;
+        Image<Bgr, byte> chapic4;
+        Image<Bgr, byte> chapic5;
+        Image<Bgr, byte> chapic6;
+        Image<Bgr, byte> chapic7;
+        Image<Gray, byte> houghpic;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        public void SetRoiRed(Image<Bgr, byte> image, Rectangle roi)//ROI选取函数
+        {
+            image.ROI = roi;
+            //image.ROI = Rectangle.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            int[,] picmat = new int[330, 250];
+            int sum;
+            int Ws=0, We=0, Hs=0, He=0;
+
+            picOrigin.Image = OriginPic.Bitmap;
+            Transferpic = OriginPic.Copy();
+            Licensepic = OriginPic.Copy();
+            
+
+            for (i = 0; i < 320; i++)
+            {
+                for (j = 0; j < 240; j++)
+                {
+                    Color pix=Transferpic.Bitmap.GetPixel(i,j);
+                    if (pix.R > 50 || pix.B < 60)
+                    {
+                        Transferpic.Bitmap.SetPixel(i, j, Color.White);
+                        picmat[i, j] = 0;
+                    }
+                    else
+                    {
+                        Transferpic.Bitmap.SetPixel(i, j, Color.Black);
+                        picmat[i, j] = 1;
+                    }
+                    
+                }
+            }
+            picRec.Image = Transferpic.Bitmap;
+
+            for (i = 50; i < 320; i++)
+            {
+                sum = 0;
+                for (j=80;j<240;j++)
+                {
+                    sum = sum + picmat[i, j];
+                }
+
+                if(sum>20)
+                {
+                    Ws = i;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            for(i=Ws;i<320;i++)
+            {
+                sum = 0;
+                for (j = 80; j < 240; j++)
+                {
+                    sum = sum + picmat[i, j];
+                }
+
+                if (sum < 2)
+                {
+                    We = i;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            for (j = 80; j < 240; j++)
+            {
+                sum = 0;
+                for (i = 50; i < 320; i++)
+                {
+                    sum = sum + picmat[i, j];
+                }
+
+                if (sum > 50)
+                {
+                    Hs = j;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            for (j = Hs; j < 240; j++)
+            {
+                sum = 0;
+                for (i = 50; i < 320; i++)
+                {
+                    sum = sum + picmat[i, j];
+                }
+
+                if (sum < 5)
+                {
+                    He = j;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if(He-Hs==0)
+            {
+                He = He + 1;
+                MessageBox.Show("System Error");
+            }
+            
+            if(We-Ws==0)
+            {
+                We = We + 1;
+                MessageBox.Show("System Error");
+            }
+
+            OriginPic.Draw(new Rectangle(Ws, Hs, We - Ws, He - Hs),new Bgr(0,0,255) , 2);
+            SetRoiRed(Licensepic, new Rectangle(Ws, Hs, We - Ws, He - Hs));
+            picOrigin.Image = OriginPic.Bitmap;
+            picLicense.Image = Licensepic.Bitmap;
+
+            
+
+            chapic1 = Licensepic.Copy();
+            chapic2 = Licensepic.Copy();
+            chapic3 = Licensepic.Copy();
+            chapic4 = Licensepic.Copy();
+            chapic5 = Licensepic.Copy();
+            chapic6 = Licensepic.Copy();
+            chapic7 = Licensepic.Copy();
+
+            SetRoiRed(chapic1, new Rectangle(0,0,(We-Ws)/7,He-Hs));
+            piccha1.Image = chapic1.Bitmap;
+
+            SetRoiRed(chapic2, new Rectangle((We - Ws) / 7, 0, (We - Ws) / 7, He - Hs));
+            piccha2.Image = chapic2.Bitmap;
+
+            SetRoiRed(chapic3, new Rectangle((We - Ws)*2 / 7+ (We - Ws)/20, 0, (We - Ws) / 7, He - Hs));
+            piccha3.Image = chapic3.Bitmap;
+
+            SetRoiRed(chapic4, new Rectangle((We - Ws) * 3 / 7 + (We - Ws) / 30, 0, (We - Ws) / 7, He - Hs));
+            piccha4.Image = chapic4.Bitmap;
+
+            SetRoiRed(chapic5, new Rectangle((We - Ws) * 4 / 7 + (We - Ws) / 30, 0, (We - Ws) / 7, He - Hs));
+            piccha5.Image = chapic5.Bitmap;
+
+            SetRoiRed(chapic6, new Rectangle((We - Ws) * 5 / 7, 0, (We - Ws) / 7, He - Hs));
+            piccha6.Image = chapic6.Bitmap;
+
+            SetRoiRed(chapic7, new Rectangle((We - Ws) * 6 / 7, 0, (We - Ws) / 7, He - Hs));
+            piccha7.Image = chapic7.Bitmap;
+
+            //切割
+            
+
+            var grayImage = Transferpic.Convert<Gray, Byte>();
+            Image<Gray, byte> Cannypic = new Image<Gray, byte>(Transferpic.Width,Transferpic.Height);
+            Image<Gray, byte> Thresholdpic = new Image<Gray, byte>(Transferpic.Width, Transferpic.Height);
+            Image<Gray, byte> Contourspic = new Image<Gray, byte>(Transferpic.Width, Transferpic.Height);
+            Image<Bgr, byte> Drawpic = new Image<Bgr, byte>(Transferpic.Width, Transferpic.Height);
+            VectorOfVectorOfPoint con1 = new VectorOfVectorOfPoint();
+            IntPtr Dynstorage = CvInvoke.cvCreateMemStorage(0);
+
+            CvInvoke.cvThreshold(grayImage, Thresholdpic , 0.0001,255.0,THRESH.CV_THRESH_OTSU);
+            CvInvoke.cvCanny(grayImage, Cannypic, 60, 100, 3);
+
+            IntPtr Dyncontour = new IntPtr();
+            MCvContour con = new MCvContour();
+            CvInvoke.cvFindContours(Thresholdpic, Dynstorage, ref Dyncontour, Marshal.SizeOf(con), RETR_TYPE.CV_RETR_TREE, CHAIN_APPROX_METHOD.CV_LINK_RUNS, new Point(0, 0));
+            CvInvoke.cvDrawContours(Contourspic, Dyncontour, new MCvScalar(255, 255, 255), new MCvScalar(255, 255, 0), 2, 1, LINE_TYPE.CV_AA, new Point(0, 0));
+
+            //待施工
+            
+            //CvInvoke.cvMinAreaRect2(Dynstorage, Contourspic);//???
+
+            picLaststep.Image = Contourspic.Bitmap;
+
+            houghpic = Contourspic.Copy();
+            IntPtr houghline = CvInvoke.cvCreateMemStorage(0);
+            ////int[,] linemat = new int[5, 1];
+            CvInvoke.cvHoughLines2(houghpic, houghline, HOUGH_TYPE.CV_HOUGH_STANDARD, 1.0, 0.01745, 1, 50, 0);
+            
+            //picLaststep.Image = houghpic.Bitmap;
+
+
+            grayImage.Dispose();
+            OriginPic.Dispose();
+            Transferpic.Dispose();
+            Cannypic.Dispose();
+            Contourspic.Dispose();
+            Drawpic.Dispose();
+        }
+    }
+}
